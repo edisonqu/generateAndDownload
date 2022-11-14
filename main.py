47d0c180c713c0bf5a1cfad84a07b0123ce5a691
edison <edisonquu@gmail.com>
2022-11-14 16:55:48 -0500

    Works with an input of a prompt and then saves to /images

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ from playwright.sync_api import sync_playwright
 def run(playwright):
     load_dotenv()
     openai.api_key = os.getenv("OPENAI_API_KEY")
+    inputted_prompt = input("Input your Prompt: ")
     response = openai.Image.create(
-        prompt="Crypto coin of a cat",
+        prompt= inputted_prompt,
         n=1,
         size="1024x1024"
     )
+
     image_url = response['data'][0]['url']  # URL of the website of the generated Art
     print(image_url)
     chromium = playwright.chromium
@@ -22,7 +24,6 @@ def run(playwright):
     page.goto(image_url)
     print(str(page.title()))
     title_html = page.title().strip("(1024×1024)")
-    # other actions...
     browser.close()
     return title_html,image_url
 
@@ -31,5 +32,5 @@ with sync_playwright() as playwright:
     img_data = requests.get(image_url).content
     with open(image_name, 'wb') as handler:
         handler.write(img_data)
-        shutil.move(image_name, '/Users/edisonqu/PycharmProjects/test/images')
+        shutil.move(image_name, 'images')
     print("Success")
